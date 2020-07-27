4068991a67e652ef1b1b03d4d70e179783c109c8
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-07-27 13:08:06 -0600

    Create SharedDigit.java

---
+++ SharedDigit.java
@@ -0,0 +1,31 @@
+/********************************************************************CODING EXERCISE 18: SHARED DIGIT***************************************************************
+DESCRIPTION EXERCISE:
+  - Write a method named hasSharedDigit with two parameters of type int. 
+  - Each number should be within the range of 10(inclusive) - 99(inclusive). If one of the numbers is not within the range, the method should return false. 
+  - The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false. 
+NOTE:
+  - The method hasSharedDigit should be defined as public static 
+**********************************************************************************CODE*****************************************************************************/
+
+public class SharedDigit {
+
+  public static void main(String [] args) {
+  
+    System.out.println(hasSharedDigit (12.23));
+  
+  }
+  
+  public static boolean hasSharedDigit(int fNumber, int sNumber) {
+  
+    if(fNumber >= 10 && fNumber <= 99 && sNumber >= 10 && sNumber >=10 && sNumber <=99 || fNumber / 10 == sNumber / 10 || fNumber %  10 = sNumber / 10){
+    
+      return true;
+      
+    }
+  
+  }
+  return false;
+  
+  }
+
+}
