475c2caf23b3ef0aa257ff8e31f7d3629e0a5113
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-02 10:57:12 -0600

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,4 +1,4 @@
-/**************************************************************CODING EXERCISE 29: SUM CALCULATOR*********************************************
+/************************************************************CODING EXERCISE 29: SIMPLE CALCULATOR*********************************************
 DESCRIPTION EXERCISE: 
 
 Write a class with the name SimpleCalculator. The class needs two fields (instance variables) with names firstNumber and secondNumber both of type double.
