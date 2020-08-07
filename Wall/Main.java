bd061004d459ca2c778c71172e47788887eb1e6d
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-07 12:50:55 -0600

    Update Main.java

--- Wall/Main.java
+++ Wall/Main.java
@@ -5,6 +5,11 @@ public class Main {
     System.out.println("area = " wall.getArea());
   
     Wall wall = new Wall(5,4);
+    
+    wall.setHeight(-1.5);
+    System.out.println("width = " + wall.getWidth());
+    System.out.println("height = " + wall.getHeight());
+    System.out.println("area = " + wall.getArea());
   
   } // main method
 
