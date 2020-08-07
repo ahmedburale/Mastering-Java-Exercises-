c25b36765d88c4c257f6844a62c54103073d5224
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-07 12:15:40 -0600

    Create Main.java

---
+++ Wall/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String [] args) {
+    
+    System.out.println("area = " wall.getArea());
+  
+    Wall wall = new Wall(5,4);
+  
+  } // main method
+
+}// main class
