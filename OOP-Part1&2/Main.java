40d561913695fa009ca18f7a4cd7ee170a2149cd
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-11 08:43:44 -0600

    Update Main.java

--- OOP-Part1&2/Main.java
+++ OOP-Part1&2/Main.java
@@ -3,3 +3,17 @@
 
 
 *******************************************************************CODE SOLUTION***************************************************************************
+package com.objectorientedprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+//        dog.eat();
+        dog.walk();
+//        dog.run();
+
+    }
+}
