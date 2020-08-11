5cf70309567835ccfde7e7363a8eb391bb2b7fc4
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-11 08:47:04 -0600

    Create Fish.java

---
+++ OOP-Part1&2/Fish.java
@@ -0,0 +1,36 @@
+package com.objectorientedprogramming;
+
+public class Fish extends Animal {
+
+    private int gills;
+    private int eyes;
+    private int fins;
+
+    public Fish(String name, int size, int weight, int gills, int eyes, int fins) {
+
+        super(name, 1, 1, size, weight);
+        this.gills = gills;
+        this.eyes = eyes;
+        this.fins = fins;
+    }
+
+    private void rest() {
+
+    }
+
+    private void moveMuscles() {
+
+    }
+
+    private void moveBackFin() {
+
+    }
+
+    private void swim(int speed) {
+        moveMuscles();
+        moveBackFin();
+        super.move(speed);
+
+
+    }
+}
