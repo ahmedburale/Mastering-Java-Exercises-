72de1e19975d32c9b8c988d696383b59f568ca26
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-14 14:50:24 -0600

    Update Rectangle.java

--- Coding-Exercise-35: Pool Area/Rectangle.java
+++ Coding-Exercise-35: Pool Area/Rectangle.java
@@ -1,1 +1,22 @@
-
+public class Rectangle {
+     
+        private double width;
+        private double length;
+     
+        public Rectangle(double width, double length) {
+            this.width = width < 0 ? 0 : width;
+            this.length = length < 0 ? 0 : length;
+        }
+     
+        public double getWidth() {
+            return width;
+        }
+        
+        public double getLength() {
+            return length;
+        }
+        
+        public double getArea() {
+            return width * length;
+        }
+    }
