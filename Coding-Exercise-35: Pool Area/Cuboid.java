d3081372ec4cf346af6c0fb68006f6a45ced9079
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-14 14:50:42 -0600

    Update Cuboid.java

--- Coding-Exercise-35: Pool Area/Cuboid.java
+++ Coding-Exercise-35: Pool Area/Cuboid.java
@@ -1,1 +1,17 @@
-
+public class Cuboid extends Rectangle {
+     
+        private double height;
+     
+        public Cuboid(double width, double length, double height) {
+            super(width, length);
+            this.height = height < 0 ? 0 : height;
+        }
+     
+        public double getHeight() {
+            return height;
+        }
+     
+        public double getVolume() {
+            return getArea() * height;
+        }
+    }
