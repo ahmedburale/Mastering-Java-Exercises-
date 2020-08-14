aac0f2ef9015c9322c6ecade3e55784f3f6da298
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-14 15:23:54 -0600

    Update Main.java

--- Coding-Exercise-35: Pool Area/Main.java
+++ Coding-Exercise-35: Pool Area/Main.java
@@ -3,9 +3,17 @@ public class Main {
     public static void main(String[] args) {
       
       // Test Code: 
-      
-      
-      
-    }
-  
+     Rectangle rectangle = new Rectangle(5, 10);
+     System.out.println("rectangle.width = " + rectangle.getWidth());
+     System.out.println("rectangle.length = " + rectangle.getLength());
+     System.out.println("rectangle.area = " + rectangle.getArea());
+     
+     Cuboid cuboid = new Cuboid(5, 10, 5);
+     System.out.println("cuboid.width = " + cuboid.getWidth());
+     System.out.println("cuboid.length = " + cuboid.getLength()); 
+     System.out.println("cuboid.area = " + cuboid.getArea());
+     System.out.println("cuboid.height " + cuboid.getHeight());
+     System.out.println("cuboid.volume " + cuboid.getVolume());
+          
+    }  
 }
