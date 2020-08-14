c34a58b990d14dad5a534a1bac0c4ee4387dbff3
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-14 10:28:41 -0600

    Create Vehicle.java

---
+++ Inheritance-Challenge/Vehicle.java
@@ -0,0 +1,54 @@
+public class Vehicle{
+
+  private String name:
+  private String size;
+  
+  private int currentVelocity;
+  private int currentDirection;
+  
+  public Vehicle(String name, String size) {
+  
+    this.name = name; 
+    this.size = size; 
+    
+    this.currentVelocity = 0;
+    this.currentDirection = 0; 
+  
+  }
+  
+  public void Steer(int direction){
+  
+    this.currentDirection += direction; 
+    System.out.println("Vehicle .steer(): Steering at " + currentDirection + " in direction " degrees.");
+  
+  }
+  
+  public void move(int velocity, int direction) {
+  
+    currentVelocity = velocity; 
+    currentDirection = direction; 
+    System.out.println("Vehicle.move(): Moving at " + currentVelocity + " in direction " + currentDirection);
+  
+  }
+  
+  public String getName() {  
+    return name;
+  }
+  
+  public String size() {
+    return size; 
+  }
+  
+  public int getCurrentVelocity() {
+    return currentVelocity;
+  }
+  
+  public int getCurrentDirection() {
+    return currentDirection; 
+  }
+  
+  public void stop() {
+    this.currentVelocity = 0;
+  }
+
+}
