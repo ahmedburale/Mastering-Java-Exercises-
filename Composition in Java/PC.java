3a5b800839d5095bb49c5038eb1955f3a8579e19
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-17 15:00:34 -0600

    Update PC.java

--- Composition in Java/PC.java
+++ Composition in Java/PC.java
@@ -1,5 +1,22 @@
 public class PC {
-
-
+  
+  private Case theCase; 
+  private Monitor monitor; 
+  private Motherboard motherboard;
+  
+  public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+    this.theCase = theCase;
+    this.monitor = monitor;
+    this.motherboard = motherboard;
+  }
+  
+  public void powerUP(){
+    theCase.pressPowerButton();
+    drawLogo();
+  }
+  public void drawLogo(){
+    //Fancy graphs
+    monitor.drawPixelAt(1200, 50, "yellow");
+  }
 
 }
