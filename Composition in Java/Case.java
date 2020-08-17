1e8fe33d6ac7badd81796d4530d26ed4be95a45a
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-17 15:28:10 -0600

    Update Case.java

--- Composition in Java/Case.java
+++ Composition in Java/Case.java
@@ -1,5 +1,31 @@
 public class Case {
-
-
-
+  private String model;
+  private String manufacturer;
+  private String powerSupply;
+  private Dimensions dimensions;
+  
+  public Case(String model, String manufacturer, String powerSupply, Dimensions dimensions){
+    this.model = model, 
+    this.manufacturer manufacturer; 
+    this.powerSupply = powerSupply;
+    this.dimensions = dimensions;
+  }
+  
+  public void pressPowerButton(){
+    System.out.println("Power button pressed");
+  }
+  
+  //getters
+  public String getModel(){
+    return model;
+  }
+  public String getManufacturer(){
+    return manufacturer;
+  }
+  public String getPowerSupply(){
+    return powerSupply;
+  }
+  public Dimensions getDimensions(){
+    return dimensions;
+  }
 }
