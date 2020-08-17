58b720f0d54b0d561331b345dc2d66a2a4a862e9
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-17 13:22:02 -0600

    Update Monitor.java

--- Composition in Java/Monitor.java
+++ Composition in Java/Monitor.java
@@ -1,5 +1,35 @@
 public class Monitor {
-
-
+  
+  private String model;
+  private String manufacturer; 
+  privat int size; 
+  private Resolution nativeResolution;
+  
+  public Monitor(String model, String manufacturer, int size, Resolution nativeResolution) {
+    this.model = model;
+    this.manufacturer = manufacturer; 
+    this.size = size;
+    this.nativeResolution = nativeResolution; 
+  }
+  
+  public void drawPixelAt(int x, int y, String color) {
+    System.out.println("Drawing pixel at " x + "," = y + " in color " + color);
+  }
+  
+  public String getModel() {
+    return model;
+  }
+  
+  public String getManufacturer() { 
+    return manufacturer;
+  }
+  
+  public int getSize() {
+    return size;
+  }
+  
+  public Resolution getNativeResolution(){
+    return nativeResolution;
+  }
 
 }
