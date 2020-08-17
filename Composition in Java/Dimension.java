45940444755ffeda230350a7a805c4931c813072
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-08-17 15:56:22 -0600

    Update Dimension.java

--- Composition in Java/Dimension.java
+++ Composition in Java/Dimension.java
@@ -1,5 +1,24 @@
 public class Dimension {
-
-
-
+  private int width;
+  private int height;
+  private int depth; 
+  
+  
+  public Dimension(int width, int height, int depth){
+    this.width = width; 
+    this.height = height;
+    this.depth = depth; 
+  }
+  
+  //getters
+  public int getWidth(){
+    return width;
+  }
+  public int getHeight(){
+    return height;
+  }
+  public int getDepth(){
+    return depth;
+  }
+  
 }
