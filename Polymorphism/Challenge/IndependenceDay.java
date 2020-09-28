e95ab83a229f125521cd5601f495545ca51ae996
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-09-28 15:37:38 -0600

    Update IndependenceDay.java

--- Polymorphism/Challenge/IndependenceDay.java
+++ Polymorphism/Challenge/IndependenceDay.java
@@ -1,1 +1,11 @@
+class IndependenceDay extends Movie {
+  public IndependenceDay() {
+    super("IndependenceDay");
+  }
+  
+  @Override
+  public String plot() {
+    return "Aliens attempt to take over planet earth";
+  }
 
+}
