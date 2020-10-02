b8349b59b20fc39bcdb93d8bf11c325664a0d271
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-10-02 15:12:35 -0600

    Update Main.java

--- Polymorphism/Challenge2/Main.java
+++ Polymorphism/Challenge2/Main.java
@@ -1,1 +1,25 @@
+public class Main {
 
+  public static void main(String [] args){
+    
+    Car car = new Car(8, "Base car")'
+    System.out.println(car.startEngine());
+    System.out.println(car.accelerate());
+    System.out.println(car.brake()); 
+    
+    Mitsubishi mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD"); 
+    System.out.println(mitsubishi.startEngine());
+    System.out.println(mitsubishi.accelerate());
+    System.out.println(mitsubishi.brake());
+    
+    Ford ford = new Ford(6, "Ford Falcon");
+    System.out.println(ford.startEngine());
+    System.out.println(ford.accelerate());
+    System.out.println(ford.brake());
+    
+    Holden holden = new Holden(6, "Holden Commodore");
+    System.out.println(holden.startEngine());
+    System.out.println(holden.accelerate());
+    System.out.println(holden.brake()); 
+  }
+}
