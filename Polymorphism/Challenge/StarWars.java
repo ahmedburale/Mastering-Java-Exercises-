18f15ee07949c71c1dcbf0532fda6e7f7581cee2
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-10-02 09:41:44 -0600

    Update StarWars.java

--- Polymorphism/Challenge/StarWars.java
+++ Polymorphism/Challenge/StarWars.java
@@ -1,1 +1,10 @@
-
+class StarWars extends Movie {
+  public StarWars(){
+    super("Star Wars);
+  }
+  @Override 
+  public String plot() {
+    return"Imperial Forces try to take over the universe";
+  }
+  
+}
