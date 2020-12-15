0a7ee8d267b0cd95b333018e4cf20b2b102be61e
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-12-14 18:25:29 -0700

    Update Main.java

--- Array/Challenge100/Main.java
+++ Array/Challenge100/Main.java
@@ -1,1 +1,13 @@
+/*
+CODE CHALLENGE DESCRIPTION: 
+  - Create a program using arrays that sorts a list of integers in descending order. 
+  - Descending orders is the highest value to lowest 
+  - In other words if the array had the valuees in it 106,26,8,5,15 your program should ultimately have an array with 106,81,26,15,5, in it. 
+  - Set up the program so that the numbers to sort are read in from the keyboard. 
+  - Implement the following methods - getIntegers, printArray, and sortIntegers
+  - getIntegers 
 
+
+
+
+*/
