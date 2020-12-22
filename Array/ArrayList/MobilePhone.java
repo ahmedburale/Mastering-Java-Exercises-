53084a333fd2dc46180a0bee2e587b5cfc7e1c20
Ahmed Burale <ahmedburale@users.noreply.github.com>
2020-12-21 20:31:20 -0700

    Update MobilePhone.java

--- Array/ArrayList/MobilePhone.java
+++ Array/ArrayList/MobilePhone.java
@@ -40,12 +40,50 @@ public class MobilePhone {
   public boolean removeContact(Contact contact) {
     int foundPosition = findContact(contact);
     if(foundPosition  < 0) {
-      System.out.println(contact.getName() + " was deleted.");
+      System.out.println(contact.getName() + " was not found.");
       return false; 
     }
+    this.myContacts.remove(foundPosition);
+    System.out.println(contact.getName() + ", was deleted.");
+    return true;
   }
   
+  //findContact method 
+  private int findContact(Contact contact){
+    return this.myContacts.indexOf(contact);
+  }
   
-
+  private int findContact(String contactName){
+    for(int i=0; i<this.myContacts.size(); i++){
+      Contact contact = this.myContacts.get(i);
+      if(contact.getName().equals(contactName)) {
+        return i;
+      }
+    }
+    return -1;
+  
+  }
+  // queryContact 
+  public String queryContact(Conctact contact) {
+    if(findContact(conctact) >= 0){
+      return contact.getName();
+    }
+    return null; 
+  }
+  
+  public Contact queryContact(String name) {
+    int position = findContact(name);
+    if(position >= 0) {
+      return this.myContacts.get(position);
+    }
+    return null;
+  }
+  
+  public void printContacts(){
+    System.out.println("Contact List");
+    for(int i=0; i<this.myContacts.size(); i++){
+      System.out.println((i+1) + "." + this.myContacts.get(i).getName() + " -> " + this.myContacts.get(i).getPhoneNumber()); 
+    }
+  }
 
 }// class MobilePhone
