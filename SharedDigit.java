bda7a1064989a54a703f3896e9486bc6ea49ae58
Modit <86144424+ModitParmar01@users.noreply.github.com>
2022-08-21 16:25:25 +0530

    Update SharedDigit.java

--- SharedDigit.java
+++ SharedDigit.java
@@ -17,7 +17,7 @@ public class SharedDigit {
   
   public static boolean hasSharedDigit(int fNumber, int sNumber) {
   
-    if(fNumber >= 10 && fNumber <= 99 && sNumber >= 10 && sNumber >=10 && sNumber <=99 || fNumber / 10 == sNumber / 10 || fNumber %  10 = sNumber / 10){
+    if(fNumber >= 10 && fNumber <= 99 && sNumber >= 10 && sNumber >=10 && sNumber <=99 || fNumber / 10 == sNumber / 10 || fNumber %  10 == sNumber / 10){
     
       return true;
       
