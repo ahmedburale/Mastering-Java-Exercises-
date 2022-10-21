f0c23b0ca3cab649b9ee0c2fc42cdd0b0c93640a
Deepak Kumar <75525675+deepakplay@users.noreply.github.com>
2022-10-21 08:29:55 +0530

    if it is summer the upper limit temperature is 45

--- PlayingCat.java
+++ PlayingCat.java
@@ -1,6 +1,6 @@
 /************************************************************CODING EXERCISE 11: PLAYING CAT************************************************************
 Description Exercise: 
-  - The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive). Unless it is summer, then 
+  - The cats spend most of the day playing. In particular, they play if the temperature is between 25 and 35 (inclusive). if it is summer, then 
     the upper limit is 45 (inclusive) instead of 35. 
   - Write a method isCatPlaying that has 2 parameters
   - Method needs to return if the cat is playing, otherwise, return false. 
